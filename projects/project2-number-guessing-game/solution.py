36a57933d8590b0dc15ba82c7e7f2b5dace01a31
adayllmaz <yilmaz.ada@ibu.edu.mk>
2026-04-22 18:59:02 +0000

    Ada: finished project 2

--- projects/project2-number-guessing-game/solution.py
+++ projects/project2-number-guessing-game/solution.py
@@ -1,16 +1,28 @@
 # Project 2 — Number Guessing Game
-# Author: your name here
+# Author: Ada
 
 import random
 
-# TODO: generate a random secret number between 1 and 10
+# generate a random secret number between 1 and 10
+secret = random.randint(1, 10)
 
-# TODO: set up a guesses counter
+# set up a guesses counter
+guesses = 0
 
-# TODO: get the user's first guess
+# get the user's first guess
+guess = int(input("Guess a number between 1 and 10: "))
 
-# TODO: while loop — keep asking until the guess is correct
-#   - print "Too low!" or "Too high!" on each wrong guess
-#   - count each guess
+# while loop — keep asking until the guess is correct
+while guess != secret:
+    guesses += 1
 
-# TODO: print the congratulations message with the number of guesses
+    if guess < secret:
+        guess = int(input("Too low! Try again: "))
+    else:
+        guess = int(input("Too high! Try again: "))
+
+# count the correct guess
+guesses += 1
+
+# print the congratulations message
+print(f"Correct! You got it in {guesses} guesses.")
